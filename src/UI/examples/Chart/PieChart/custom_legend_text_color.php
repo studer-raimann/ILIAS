1eb09e5ab5b45f9eb345d21f1d2cacd9e8531a65
jdolf <jd@studer-raimann.ch>
2019-07-08 12:15:54 +0200

    Adjust examples

--- src/UI/examples/Chart/PieChart/custom_legend_text_color.php
+++ src/UI/examples/Chart/PieChart/custom_legend_text_color.php
@@ -6,13 +6,13 @@ use ILIAS\UI\Implementation\Component\Chart\PieChart\PieChartItem;
 /**
  * @return string
  */
-function custom_total_value(): string {
+function custom_legend_text_color(): string {
 	global $DIC;
 
 	$c = $DIC->ui()->factory()->chart()->pieChart([
 		new PieChartItem("One", [5.4], new Color(55, 0, 0), new Color(200, 200, 200)),
 		new PieChartItem("Two", [12], new Color(0, 200, 0)),
-	])->withCustomTotalValue(2);
+	])->withCustomLegendTextColor(new Color(0, 0, 255));
 
 	return $DIC->ui()->renderer()->render($c);
 }
--- src/UI/examples/Chart/PieChart/not_show_legend.php
+++ src/UI/examples/Chart/PieChart/not_show_legend.php
@@ -10,8 +10,8 @@ function not_show_legend(): string {
 	global $DIC;
 
 	$c = $DIC->ui()->factory()->chart()->pieChart([
-		new PieChartItem("One", 5.4, new Color(55, 0, 0), new Color(200, 200, 200)),
-		new PieChartItem("Two", 12, new Color(0, 200, 0)),
+		new PieChartItem("One", [5.4], new Color(55, 0, 0), new Color(200, 200, 200)),
+		new PieChartItem("Two", [12], new Color(0, 200, 0)),
 	])->withShowLegend(false);
 
 	return $DIC->ui()->renderer()->render($c);
