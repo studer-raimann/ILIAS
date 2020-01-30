c4dd2d24bc9cb2d32e260168c0ef6a27f4418436
jdolf <jd@studer-raimann.ch>
2020-01-30 12:07:38 +0100

    Move DB update steps to custom file

---
+++ setup/sql/dbupdate_custom.php
@@ -0,0 +1,106 @@
+<#1>
+<?php
+if (!$ilDB->tableColumnExists('il_dcl_tview_set', 'default_value')) {
+    $ilDB->addTableColumn(
+        'il_dcl_tview_set',
+        'default_value',
+        array(
+            'type'    => 'text',
+            'length'  => 255,
+            'notnull' => false
+        )
+    );
+}
+?>
+<#2>
+<?php
+if (!$ilDB->tableColumnExists('il_dcl_tview_set', 'required')) {
+    $ilDB->addTableColumn(
+        'il_dcl_tview_set',
+        'required',
+        array(
+            'type'    => 'integer',
+            'length'  => 1,
+            'notnull' => true,
+            'default' => 0
+        )
+    );
+}
+?>
+<#3>
+<?php
+if (!$ilDB->tableColumnExists('il_dcl_tview_set', 'locked')) {
+    $ilDB->addTableColumn(
+        'il_dcl_tview_set',
+        'locked',
+        array(
+            'type'    => 'integer',
+            'length'  => 1,
+            'notnull' => true,
+            'default' => 0
+        )
+    );
+}
+?>
+<#4>
+<?php
+if (!$ilDB->tableColumnExists('il_dcl_tview_set', 'visible_create')) {
+    $ilDB->addTableColumn(
+        'il_dcl_tview_set',
+        'visible_create',
+        array(
+            'type'    => 'integer',
+            'length'  => 1,
+            'notnull' => true,
+            'default' => 1
+        )
+    );
+}
+?>
+<#5>
+<?php
+if (!$ilDB->tableColumnExists('il_dcl_tview_set', 'visible_edit')) {
+    $ilDB->addTableColumn(
+        'il_dcl_tview_set',
+        'visible_edit',
+        array(
+            'type'    => 'integer',
+            'length'  => 1,
+            'notnull' => true,
+            'default' => 1
+        )
+    );
+}
+?>
+<#6>
+<?php
+if ($ilDB->tableColumnExists('il_dcl_field', 'required')) {
+    // Migration
+    $res = $ilDB->query("SELECT id, required FROM il_dcl_field");
+    while ($rec = $ilDB->fetchAssoc($res)) {
+        $ilDB->queryF(
+            "UPDATE il_dcl_tview_set SET required = %s WHERE field = %s",
+            array('integer', 'text'),
+            array($data['required'], $data['id'])
+        );
+    }
+
+    $ilDB->dropTableColumn('il_dcl_field', 'required');
+}
+?>
+<#7>
+<?php
+if ($ilDB->tableColumnExists('il_dcl_field', 'is_locked')) {
+    // Migration
+    $res = $ilDB->query("SELECT id, is_locked FROM il_dcl_field");
+    while ($rec = $ilDB->fetchAssoc($res)) {
+        $ilDB->queryF(
+            "UPDATE il_dcl_tview_set SET locked = %s WHERE field = %s",
+            array('integer', 'text'),
+            array($data['is_locked'], $data['id'])
+        );
+    }
+
+    $ilDB->dropTableColumn('il_dcl_field', 'is_locked');
+}
+?>
