f2477fc7a04f10277a6ded42478d177eb174c5dd
Nicolas Schaefli <ns@studer-raimann.ch>
2021-03-18 14:01:48 +0100

    Add db migration for etalk data table

--- setup/sql/dbupdate_05.php
+++ setup/sql/dbupdate_05.php
@@ -4171,3 +4171,36 @@ $res = $ilDB->manipulateF(
 $tree = new ilTree(ROOT_FOLDER_ID);
 $tree->insertNode($ref_id, SYSTEM_FOLDER_ID);
 ?>
+<#5651>
+<?php
+try {
+    if ($ilDB->supportsTransactions()) {
+        $ilDB->beginTransaction();
+    }
+    $etalTableName = 'etal_data';
+
+    $ilDB->createTable($etalTableName, [
+        'object_id' => ['type' => 'integer', 'length' => 8, 'notnull' => true],
+        'series_id' => ['type' => 'text', 'length' => 36, 'notnull' => true, 'fixed' => true],
+        'start_date' => ['type' => 'integer', 'length' => 8, 'notnull' => true],
+        'end_date' => ['type' => 'integer', 'length' => 8, 'notnull' => true],
+        'all_day' => ['type' => 'integer', 'length' => 1, 'notnull' => true],
+        'employee' => ['type' => 'integer', 'length' => 8, 'notnull' => true],
+        'location' => ['type' => 'text', 'length' => 200, 'notnull' => false, 'fixed' => false],
+        'completed' => ['type' => 'integer', 'length' => 1, 'notnull' => true]
+    ]);
+
+    $ilDB->addPrimaryKey($etalTableName, ['object_id']);
+    $ilDB->addIndex($etalTableName, ['series_id'], 'ser');
+    $ilDB->addIndex($etalTableName, ['employee'], 'emp');
+
+    if ($ilDB->supportsTransactions()) {
+        $ilDB->commit();
+    }
+} catch (\Exception $exception) {
+    if ($ilDB->supportsTransactions()) {
+        $ilDB->rollback();
+    }
+    throw $exception;
+}
+?>
